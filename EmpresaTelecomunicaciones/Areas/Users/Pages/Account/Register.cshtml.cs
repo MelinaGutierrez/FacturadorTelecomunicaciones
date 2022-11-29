82279d34a7145a91f0a9da3dfb227231432be9fc
Melina <melina.gutierrez@ucb.edu.bo>
2022-11-29 15:36:14 -0400

    Avance

---
+++ EmpresaTelecomunicaciones/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmpresaTelecomunicaciones.Areas.Users.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmpresaTelecomunicaciones.Areas.Users.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public class InputModel : InputModelRegister
+        {
+
+        }
+    }
+}
